2b765a15087478064ea9f3393c44fbcd83ee75be
Dannymane <daniel.yanko5@gmail.com>
2023-10-05 13:59:02 +0200

    Added yield

--- Note1.cs
+++ Note1.cs
@@ -9,14 +9,30 @@ public static class StaticClass{
    }
 }
 public class Program{
-   public static void Maine(){
+   public static IEnumerable<int> IteratiorMethod()
+   {
+      yield return 0;
+      yield return 1;
+      yield return 2;
+   }
+   public static void Mainw(){
       Console.WriteLine(Guid.NewGuid().ToString()); //7497599a-d2fc-43bc-84b1-ed67156f7672
 
       //Can not create an instance of the static class 
       // StaticClass sc = new StaticClass();
       // sc.Test();
 
-      //StringBuilder
-      //Regex
-   }
+
+      foreach(int i in Program.IteratiorMethod())
+      {
+            Console.WriteLine(i);
+      }
+
+      var array = Program.IteratiorMethod().ToArray();
+      Console.WriteLine(array[2]);
+
+
+        //StringBuilder
+        //Regex
+    }
 }
