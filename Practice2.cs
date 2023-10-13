5e4b7349bb003d27b9f6b18fd6d78f4dfe5a9f44
Dannymane <daniel.yanko5@gmail.com>
2023-10-13 09:21:36 +0200

    Practicing

--- Practice2.cs
+++ Practice2.cs
@@ -8,25 +8,8 @@ public class Practice2{
 
 public static void Main(string[] args){
    
-   var sb = new StringBuilder("Hello World", 40);
-   sb.Append("!");
-
-   sb.Insert(5, " C#");
-   Console.WriteLine(sb); // Hello C# World!
-
-   string str = sb.ToString();
-
-   sb.Remove(8, sb.Length - 8);
+   var sb = new StringBuilder("Hello New York", 20);
+   sb.Replace("Hello ", "");
    Console.WriteLine(sb);
-
-   var list = sb.ToString().Split(" ").ToList();
-   list.ForEach(str => Console.WriteLine(str));
-
-   var sb2 = new StringBuilder("Hello beautiful World!");
-   Console.WriteLine(sb2.Remove(6,10));
-
-   Console.WriteLine(sb2.Replace("Hello", "Goodbye"));
-
-   
 }
 }
