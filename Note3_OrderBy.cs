584720661044d194db7bdfacb004c26a09108ab6
Daniel Yanko <daniel.yanko@ms.bayt.pl>
2023-12-04 12:28:53 +0100

    notes

---
+++ Note3_OrderBy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes1
+{
+    public class Note3_OrderBy
+    {
+        public static void Main(string[] args)
+        {
+            var n1 = new List<decimal?>{ 10, -9, null, 1, 0, null };
+            Console.WriteLine(String.Join(' ', n1.OrderBy(n => Math.Abs(n ?? 0))));
+        }
+    }
+}
