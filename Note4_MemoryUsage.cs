36c34aeebe4e106a0b97db6b9fca7a2e4be4f7db
Daniel Yanko <daniel.yanko@ms.bayt.pl>
2023-12-05 09:43:53 +0100

    notes

---
+++ Note4_MemoryUsage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes1
+{
+    public class Note4_MemoryUsage
+    {
+        public static void Main(string[] args)
+        {
+            //(true) forces garbage collection - so don't forget to "use" tested element in furhter code
+            long currentMemoryUsage = System.GC.GetTotalMemory(true);
+        }
+    }
+}
