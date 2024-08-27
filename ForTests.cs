c855f340b60f968769df9f9c698ff04760e222d2
Daniel Yanko <daniel.yanko@ms.bayt.pl>
2024-08-27 15:12:28 +0200

    notes

---
+++ ForTests.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace MyNamespace
+{
+    public class TravelRoute
+    {
+        public string StartCity { get; set; }
+        public string[] Waypoints { get; set; }
+
+        public TravelRoute(string startCity = null, string waypoints)
+        {
+            StartCity = startCity;
+            Waypoints = waypoints;
+        }
+
+        public void PrintRoute()
+        {
+            Console.WriteLine("Start City: " + StartCity);
+            Console.WriteLine("Waypoints:");
+            foreach (var waypoint in Waypoints)
+            {
+                Console.WriteLine(waypoint);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a TravelRoute object with waypoints but no start city
+            TravelRoute route = new TravelRoute(new List<string>() { "1", "2", "3" });
+
+            // Print the route information
+            route.PrintRoute();
+        }
+    }
+}
--- obj/Debug/net6.0/Notes1.AssemblyInfo.cs
+++ obj/Debug/net6.0/Notes1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e8d7c3a681bb84f05a549d8d27de30470daa96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a897c0d71b56b3990328738c882736d973dff514")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
