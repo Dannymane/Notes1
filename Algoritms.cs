e016953dac07fb9f2f005bdf278aa813b1270bd2
Dannymane <daniel.yanko5@gmail.com>
2024-11-16 21:59:05 +0200

    notes

--- Algoritms.cs
+++ Algoritms.cs
@@ -16,4 +16,25 @@ public class Solution {
         }
         return -1;
     }
+
+    public IList<int> InorderTraversal(TreeNode root) {
+        if(root == null)
+            return new List<int>();
+
+        var list = new List<int>();
+        list.AddRange(InorderTraversal(root.left));
+        list.Add(root.val);
+        list.AddRange(InorderTraversal(root.right));
+        return list;
+    }
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
