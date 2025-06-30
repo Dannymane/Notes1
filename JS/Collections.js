44f843abd10f18892ec85587fec7826182a1c2eb
Dannymane <daniel.yanko5@gmail.com>
2025-06-30 09:48:11 +0200

    n

---
+++ JS/Collections.js
@@ -0,0 +1,9 @@
+//Arrays
+const cars = ["Saab", "Volvo", "BMW"];
+
+const cars2 = [];
+cars2[0]= "Saab";
+cars2[1]= "Volvo";
+cars2[2]= "BMW";
+
+const cars3 = new Array("Saab", "Volvo", "BMW");
--- JS/Functions.js
+++ JS/Functions.js
@@ -9,10 +9,38 @@ function sayHi() {
 let greeter = sayHi; // Assigning function to variable
 greeter(); // Calls sayHi
 
+let a = add(5,5);
+function add(a, b) { //functions hoisted to the top of the scope
+    return a + b;
+}
 
-
-let a = multiply(2, 2); //error
-const multiply = function(a, b) { //function expression
+let a1 = multiply(2, 2); //error
+const multiply = function(a, b) { //function expressions don't hoisted
     return a * b;
 };
+
+function counter() {}
+counter.value = 0;
+console.log(counter.value);
+
+let str1 = new Object("0");
+let str2 = new Object("0");
+let zero = 0;
+console.log(str1 == zero);              // true
+console.log(str2 == zero);                                  // true
+console.log(str1 == str2);              // false
+
+//"params" in JS
+function sum(...numbers) {
+    let sum = 0; //jest let sum; won't work;
+    for(let i = 0;i < numbers.length;i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
+console.log(sum(1, 2, 3, 4)); // 10
+
+//setTimeout is a JS function that runs the passed function after delay (ms)
+setTimeout(() => console.log("Delayed"), 1000); //doesn't block the program, just execute this later
+
 
