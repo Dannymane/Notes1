d2e2dd423014f1203d4cedbb20a26f8e81c52ec2
Dannymane <daniel.yanko5@gmail.com>
2025-07-28 18:48:48 +0200

    n

--- JS/Async, promises.js
+++ JS/Async, promises.js
@@ -1,7 +1,14 @@
+//setTimeout is a JS function that runs the passed function after delay (ms)
+setTimeout(() => console.log("Delayed"), 1000); //doesn't block the program, just execute this later
+
+const promise0 = new Promise((resolve, reject) => {
+        resolve("Done!");  // marks as successful or reject("Error!") - marks as failed
+});
+
 const promise = new Promise((resolve, reject) => {
     // async work here
     setTimeout(() => {
-        resolve("Done!");  // marks as successful or reject("Error!") - marks as failed
+        resolve("Done!"); 
     }, 1000);
 });
 
@@ -42,8 +49,9 @@ console.log("End");
 // End 				 <-- after 1 s
 // Done!
 
-// Current stack, microtask queue, macrotask queue
+// Current stack, microtask queue, macrotask queue 
 console.log("Start");
+setTimeout(() => Promise.resolve().then(() => console.log("setTimeout Promise.then")), 0);
 setTimeout(() => console.log("setTimeout"), 0); //schedules it to macrotask queue - runs after microtask queue
 Promise.resolve().then(() => console.log("Promise.then"));
 console.log("End");
@@ -52,5 +60,6 @@ console.log("End");
 // Start
 // End
 // Promise.then
+// setTimeout Promise.then
 // setTimeout
 
