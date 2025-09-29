726ef1e7f2abf375f8bf07a170407340de517cd6
Dannymane <daniel.yanko5@gmail.com>
2025-09-29 08:58:27 +0200

    n

--- Angular/Projects/sandbox-app/sandbox/src/app/app.ts
+++ Angular/Projects/sandbox-app/sandbox/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { User } from './modules/main/sandbox/components/user/user';
 
+type Visivility = 'visible' | 'hidden';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, User],
@@ -9,14 +10,19 @@ import { User } from './modules/main/sandbox/components/user/user';
   styleUrl: './app.scss'
 })
 export class App {
+
   protected readonly title = signal('sandbox');
   city = "Vinnytsia";
   isServerRunning = true;
+  divVisibility: Visivility = 'visible';
 
   changeCity() {
     this.city = "Kyiv";
     this.title.set("Kyiv signal");
     this.isServerRunning = !this.isServerRunning;
+    this.divVisibility = this.divVisibility === 'visible' ? 'hidden' : 'visible';
   }
+
+  protected users = [{name: 'John', id: 0}, {name: 'Alice', id: 1}, {name: 'Bob', id: 2}];
 }
 
